b804722cd3cd5ff88862419117884e2070bc6d56
Roger Trullo <rogertrullo@hotmail.com>
2020-09-09 20:34:25 +0200

    Update nearest_neighbor.py

--- codes/nearest_neighbor.py
+++ codes/nearest_neighbor.py
@@ -16,16 +16,24 @@ def search_NN(test_emb, train_emb_flat, NN=1, method='kdt'):
     Ntest, I, J, D = test_emb.shape
     closest_inds = np.empty((Ntest, I, J, NN), dtype=np.int32)
     l2_maps = np.empty((Ntest, I, J, NN), dtype=np.float32)
-
-    for n in range(Ntest):
-        for i in range(I):
+    ######Roger###############
+    test_emb_flat=test_emb.reshape(-1,D)
+    
+    dists, inds = kdt.query(test_emb_flat, return_distance=True, k=NN)#N,1
+    closest_inds=inds.reshape(Ntest, I, J, NN)
+    l2_maps=dists.reshape(Ntest, I, J, NN)
+    ################################
+    
+    
+#     for n in range(Ntest):
+#         for i in range(I):
             
-            dists, inds = kdt.query(test_emb[n, i, :, :], return_distance=True, k=NN)
-            if n==0 and n ==0:
-                print('query shape',test_emb[n, i, :, :].shape)
-                print('dists shape',dists.shape, 'inds shape',inds.shape)
-            closest_inds[n, i, :, :] = inds[:, :]
-            l2_maps[n, i, :, :] = dists[:, :]
+#             dists, inds = kdt.query(test_emb[n, i, :, :], return_distance=True, k=NN)
+#             if n==0 and n ==0:
+#                 print('query shape',test_emb[n, i, :, :].shape)
+#                 print('dists shape',dists.shape, 'inds shape',inds.shape)
+#             closest_inds[n, i, :, :] = inds[:, :]
+#             l2_maps[n, i, :, :] = dists[:, :]
 
     return l2_maps, closest_inds
 
